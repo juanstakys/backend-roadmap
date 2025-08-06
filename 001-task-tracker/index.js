53bec76c92b889390a2d2e3f2ddcacfa01daf003
Juan Cruz Stakys <52844841+juanstakys@users.noreply.github.com>
2025-08-06 19:14:25 -0300

    001: Using array destructuring to limit the amount of arguments

--- 001-task-tracker/index.js
+++ 001-task-tracker/index.js
@@ -3,28 +3,26 @@
 import { argv } from "process";
 import { Task, taskList } from "./TaskList.js";
 
-const MAX_ARGS = 5; // TODO: make it depend on the chosen action. add: 1, update: 2, delete: 1, mark: 2, list: 1.
 const VALID_ACTIONS = {
-  add: (description) => {
+  add: ([description]) => {
     const newId = taskList.getLastId() + 1;
     const task = new Task(newId, description);
     taskList.addTask(task);
     console.log(`Task added successfully (ID: ${newId})`);
   },
-  update: (task) => {
-    const [id, ...description] = task.split(" ");
-    taskList.updateTask(id, description.join(" "));
+  update: ([id, description]) => {
+    console.log("description:", description);
+    taskList.updateTask(id, description);
     console.log(`Task updated successfully`);
   },
-  delete: (id) => {
+  delete: ([id]) => {
     taskList.deleteTask(+id);
     console.log(`Deleted task id ${id}`);
   },
-  list: (status) => {
+  list: ([status]) => {
     taskList.listTasks(status);
   },
-  mark: (task) => {
-    const [status, id] = task.split(" ", 2);
+  mark: ([status, id]) => {
     try {
       taskList.markTask(id, status);
       console.log(`Task marked ${status}`);
@@ -36,9 +34,8 @@ const VALID_ACTIONS = {
 
 // Argument parsing and validation
 const action_name = argv[2];
-const action_info = argv.slice(3).join(" ");
+const action_info = argv.slice(3);
 
-if (argv.length > MAX_ARGS) throw `Too many arguments.`;
 if (!Object.keys(VALID_ACTIONS).includes(action_name))
   throw `Invalid action name: ${action_name}.`;
 
