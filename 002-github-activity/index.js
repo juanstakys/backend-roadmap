2bf3d00640b3265d7bf84bd083f2484b4011367a
Juan Cruz Stakys <52844841+juanstakys@users.noreply.github.com>
2025-08-07 23:22:49 -0300

    Extract event types from GitHub activity response

--- 002-github-activity/index.js
+++ 002-github-activity/index.js
@@ -7,5 +7,6 @@ const [, , user, ...rest] = argv;
 console.log("Fetching data from user " + user);
 
 const response = await fetch(`https://api.github.com/users/${user}/events`);
+const rawData = JSON.parse(await response.text());
 
-console.log(JSON.parse(await response.text()));
+console.log(rawData.map((i) => i.type));
